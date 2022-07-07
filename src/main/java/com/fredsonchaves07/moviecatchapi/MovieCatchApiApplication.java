742f38bcc6330f855d5e80e5c8eb05b6cf1df008
fredsonchaves07 <fredson.ribeiro@objective.com.br>
2022-07-07 16:14:00 -0300

    feat: initial project
    Signed-off-by: fredsonchaves07 <fredson.ribeiro@objective.com.br>

---
+++ src/main/java/com/fredsonchaves07/moviecatchapi/MovieCatchApiApplication.java
@@ -0,0 +1,13 @@
+package com.fredsonchaves07.moviecatchapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MovieCatchApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MovieCatchApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/fredsonchaves07/moviecatchapi/MovieCatchApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fredsonchaves07.moviecatchapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovieCatchApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
