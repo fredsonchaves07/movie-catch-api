f6129eef9b90af93f314ce27a122b3a69f74f2f1
fredsonchaves07 <fredsonchaves07@gmail.com>
2022-11-11 10:30:58 -0300

    fix: add constraint notnull email field
    Signed-off-by: Fredson Chaves <fredsonchaves07@gmail.com>

--- src/main/java/com/fredsonchaves07/moviecatchapi/domain/entities/User.java
+++ src/main/java/com/fredsonchaves07/moviecatchapi/domain/entities/User.java
@@ -22,7 +22,7 @@ public class User {
     @Column(nullable = false)
     private String name;
 
-    @Column(unique = true)
+    @Column(nullable = false, unique = true)
     private String email;
 
     @Column(nullable = false)
