e180ad67cd5ee24b38b2f7d84e95b6244bfcf826
fredsonchaves07 <fredsonchaves07@gmail.com>
2022-11-29 19:55:04 -0300

    feat: confirm user implementation
    Signed-off-by: Fredson Chaves <fredsonchaves07@gmail.com>

--- src/main/java/com/fredsonchaves07/moviecatchapi/domain/useCases/user/ConfirmUserUseCase.java
+++ src/main/java/com/fredsonchaves07/moviecatchapi/domain/useCases/user/ConfirmUserUseCase.java
@@ -1,40 +1,51 @@
 package com.fredsonchaves07.moviecatchapi.domain.useCases.user;
 
-import com.fredsonchaves07.moviecatchapi.domain.dto.user.UserDTO;
+import com.fredsonchaves07.moviecatchapi.domain.dto.token.TokenDTO;
 import com.fredsonchaves07.moviecatchapi.domain.entities.User;
 import com.fredsonchaves07.moviecatchapi.domain.repositories.UserRepository;
+import com.fredsonchaves07.moviecatchapi.domain.service.exception.TokenException;
 import com.fredsonchaves07.moviecatchapi.domain.service.token.TokenService;
+import com.fredsonchaves07.moviecatchapi.domain.useCases.exceptions.TokenExpiredOrInvalidException;
 import com.fredsonchaves07.moviecatchapi.domain.useCases.exceptions.UserAlreadyConfirmedException;
 import com.fredsonchaves07.moviecatchapi.domain.useCases.exceptions.UserNotFoundException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Transactional;
 
+@Component
 public class ConfirmUserUseCase {
 
+    @Autowired
     private UserRepository userRepository;
 
+    @Autowired
     private TokenService tokenService;
 
     private User user;
 
-    public ConfirmUserUseCase(UserRepository userRepository, TokenService tokenService) {
-        this.userRepository = userRepository;
-        this.tokenService = tokenService;
-    }
-
     @Transactional
-    public void execute(UserDTO userDTO) {
-        String teste = tokenService.encrypt(userDTO);
-        user = userRepository.findByEmail(userDTO.getEmail());
-        if (userIsNotFound()) throw new UserNotFoundException();
-        if (isConfirmed()) throw new UserAlreadyConfirmedException();
-        user.confirmUser();
+    public void execute(TokenDTO token) {
+        try {
+            user = getUserDecriptedByToken(token);
+            if (userIsNotFound()) throw new UserNotFoundException();
+            if (isConfirmed()) throw new UserAlreadyConfirmedException();
+            user.confirmUser();
+            userRepository.save(user);
+        } catch (TokenException tokenException) {
+            throw new TokenExpiredOrInvalidException();
+        }
     }
 
-    public boolean isConfirmed() {
+    private User getUserDecriptedByToken(TokenDTO token) {
+        String email = tokenService.decrypt(token);
+        return userRepository.findByEmail(email);
+    }
+
+    private boolean isConfirmed() {
         return user.isConfirm();
     }
 
-    public boolean userIsNotFound() {
+    private boolean userIsNotFound() {
         return user == null;
     }
 }
--- src/test/java/com/fredsonchaves07/moviecatchapi/domain/useCases/user/ConfirmUserUseCaseTest.java
+++ src/test/java/com/fredsonchaves07/moviecatchapi/domain/useCases/user/ConfirmUserUseCaseTest.java
@@ -1,65 +1,99 @@
 package com.fredsonchaves07.moviecatchapi.domain.useCases.user;
 
-import com.fredsonchaves07.moviecatchapi.api.services.email.FakeSendMailService;
-import com.fredsonchaves07.moviecatchapi.api.services.token.JwtTokenService;
+import com.fredsonchaves07.moviecatchapi.domain.dto.token.TokenDTO;
 import com.fredsonchaves07.moviecatchapi.domain.dto.user.UserDTO;
 import com.fredsonchaves07.moviecatchapi.domain.repositories.UserRepository;
-import com.fredsonchaves07.moviecatchapi.domain.service.mail.SendEmailService;
 import com.fredsonchaves07.moviecatchapi.domain.service.token.TokenService;
+import com.fredsonchaves07.moviecatchapi.domain.useCases.exceptions.TokenExpiredOrInvalidException;
 import com.fredsonchaves07.moviecatchapi.domain.useCases.exceptions.UserAlreadyConfirmedException;
 import com.fredsonchaves07.moviecatchapi.domain.useCases.exceptions.UserNotFoundException;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.context.SpringBootTest;
 
 import static com.fredsonchaves07.moviecatchapi.factories.UserFactory.createUserDTO;
 import static com.fredsonchaves07.moviecatchapi.factories.UserFactory.userDTO;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
-@DataJpaTest
+@SpringBootTest
 public class ConfirmUserUseCaseTest {
 
     @Autowired
     private UserRepository userRepository;
 
+    @Autowired
     private ConfirmUserUseCase confirmUserUseCase;
 
+    @Autowired
     private CreateUserUseCase createUserUseCase;
+
+    @Autowired
+    private TokenService tokenService;
 
     private UserDTO userDTO;
 
+    private TokenDTO tokenDTO;
+
     @BeforeEach
     public void setUp() {
-        TokenService tokenService = new JwtTokenService();
-        SendEmailService sendEmailService = new FakeSendMailService();
-        confirmUserUseCase = new ConfirmUserUseCase(userRepository, tokenService);
-        createUserUseCase = new CreateUserUseCase(userRepository, sendEmailService);
+        userRepository.deleteAll();
+        userDTO = createUserUseCase.execute(createUserDTO());
+        tokenDTO = tokenService.encrypt(userDTO);
     }
 
     @Test
     public void shouldConfirmUser() {
-        userDTO = createUserUseCase.execute(createUserDTO());
-        confirmUserUseCase.execute(userDTO);
+        confirmUserUseCase.execute(tokenDTO);
         assertTrue(true);
     }
 
     @Test
     public void notShouldConfirmUserIfUserIsConfirmed() {
-        userDTO = createUserUseCase.execute(createUserDTO());
-        confirmUserUseCase.execute(userDTO);
+        confirmUserUseCase.execute(tokenDTO);
         assertThrows(
                 UserAlreadyConfirmedException.class,
-                () -> confirmUserUseCase.execute(userDTO)
+                () -> confirmUserUseCase.execute(tokenDTO)
         );
     }
 
     @Test
     public void notShoulConfirmUserIfUserIsNotFound() {
+        tokenDTO = tokenService.encrypt(userDTO("usertest2", "usertest2@email.com"));
         assertThrows(
                 UserNotFoundException.class,
-                () -> confirmUserUseCase.execute(userDTO())
+                () -> confirmUserUseCase.execute(tokenDTO)
+        );
+    }
+
+    @Test
+    public void notShouldConfirmUserIfTokenIsNull() {
+        assertThrows(
+                TokenExpiredOrInvalidException.class,
+                () -> confirmUserUseCase.execute(null)
+        );
+    }
+
+    @Test
+    public void notShouldConfirmUserWithExpiredToken() {
+        TokenDTO expiredToken = new TokenDTO("eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzUxMiJ9." +
+                "eyJzdWIiOiJ1c2VyQGVtYWlsLmNvbSIsImV4cCI6MTY2OTQ3NDkwOSwiaWF0IjoxNjY5NDc0OTA5fQ." +
+                "bNd5hND3oUbUHLrrBtXZ7w-m6reqyRVyT-TlS92_yoi4zNHH6FdsXppJtQtGhm06LgihSt9PbGjZG4SydpA4mg");
+        assertThrows(
+                TokenExpiredOrInvalidException.class,
+                () -> confirmUserUseCase.execute(expiredToken)
+        );
+    }
+
+    @Test
+    public void notShouldConfirmUserWithInvalidToken() {
+        TokenDTO invalidToken = new TokenDTO("eyJ0eXAiOiJKV1QiLCJhbGciaiJIUzUxMiJ9." +
+                "eyJzdWIiOiJ1c2VyQGVtYWlsLmNvbSIsImV4cCI6eTY2OTQ3MzMyM30." +
+                "Bb-HcCS0EYGvczEmZgxjnz-_jo9hVDfecmAMLBuSQzgHefBv-EZCKXkb8F8GEmcgvTw6By0fwiwPz8ooz5mwwg");
+        assertThrows(
+                TokenExpiredOrInvalidException.class,
+                () -> confirmUserUseCase.execute(invalidToken)
         );
     }
 }
