f281776c9b4670575b11a5d1cd9c2055da47653b
fredsonchaves07 <fredsonchaves07@gmail.com>
2022-12-06 21:05:21 -0300

    feat: confirm user api documentation

--- src/main/java/com/fredsonchaves07/moviecatchapi/api/resources/user/ConfirmUserApiResource.java
+++ src/main/java/com/fredsonchaves07/moviecatchapi/api/resources/user/ConfirmUserApiResource.java
@@ -2,6 +2,11 @@ package com.fredsonchaves07.moviecatchapi.api.resources.user;
 
 import com.fredsonchaves07.moviecatchapi.api.services.user.ConfirmUserApiService;
 import com.fredsonchaves07.moviecatchapi.domain.dto.user.UserDTO;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -12,12 +17,18 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/v1/users/confirm")
+@Api(tags = {"User"})
+@Tag(name = "User", description = "User Resource")
 public class ConfirmUserApiResource {
 
     @Autowired
     private ConfirmUserApiService confirmUserApiService;
 
     @PutMapping("/{token}")
+    @ApiOperation(value = "Confirm a user")
+    @ApiResponses(value = {
+            @ApiResponse(code = 400, message = "Bad request")
+    })
     public ResponseEntity<UserDTO> confirmUser(@PathVariable String token) {
         UserDTO userDTO = confirmUserApiService.execute(token);
         return ResponseEntity.status(HttpStatus.OK).body(userDTO);
