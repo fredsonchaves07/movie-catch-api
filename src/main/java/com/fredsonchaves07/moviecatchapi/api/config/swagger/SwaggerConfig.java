7140528a3d812f092d1d7d4d2439b536abe05509
fredsonchaves07 <fredsonchaves07@gmail.com>
2023-01-30 19:38:10 -0300

    refact: package config

--- src/main/java/com/fredsonchaves07/moviecatchapi/api/config/swagger/SwaggerConfig.java
+++ src/main/java/com/fredsonchaves07/moviecatchapi/api/config/swagger/SwaggerConfig.java
@@ -1,4 +1,4 @@
-package com.fredsonchaves07.moviecatchapi.api.config;
+package com.fredsonchaves07.moviecatchapi.api.config.swagger;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
