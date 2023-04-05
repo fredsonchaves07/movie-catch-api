f764a1ebf7a7e2c82cae01b7fb0533f63ee0d894
fredsonchaves07 <fredsonchaves07@gmail.com>
2023-04-05 09:51:30 -0300

    feat: recovery password with criteria password and exceptions

--- src/main/java/com/fredsonchaves07/moviecatchapi/domain/useCases/authentication/RecoveryPasswordUseCase.java
+++ src/main/java/com/fredsonchaves07/moviecatchapi/domain/useCases/authentication/RecoveryPasswordUseCase.java
@@ -3,10 +3,12 @@ package com.fredsonchaves07.moviecatchapi.domain.useCases.authentication;
 import com.fredsonchaves07.moviecatchapi.domain.dto.authentication.RecoveryPasswordDTO;
 import com.fredsonchaves07.moviecatchapi.domain.dto.user.UserDTO;
 import com.fredsonchaves07.moviecatchapi.domain.entities.User;
+import com.fredsonchaves07.moviecatchapi.domain.exceptions.EmailOrPasswordInvalidException;
 import com.fredsonchaves07.moviecatchapi.domain.exceptions.UnconfirmedUserException;
 import com.fredsonchaves07.moviecatchapi.domain.exceptions.UserNotFoundException;
 import com.fredsonchaves07.moviecatchapi.domain.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -16,10 +18,16 @@ public class RecoveryPasswordUseCase {
     @Autowired
     private UserRepository userRepository;
 
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
     @Transactional
     public UserDTO execute(RecoveryPasswordDTO recoveryPasswordDTO) {
         User user = getUserByRecoverPasswordDTO(recoveryPasswordDTO);
         user.setPassword(recoveryPasswordDTO.getPassword());
+        if (!user.isEmailAndPasswordValid())
+            throw new EmailOrPasswordInvalidException();
+        user.setPassword(passwordEncoder.encode(recoveryPasswordDTO.getPassword()));
         return new UserDTO(user);
     }
 
--- src/test/java/com/fredsonchaves07/moviecatchapi/domain/useCases/authentication/RecoveryPasswordUseCaseTest.java
+++ src/test/java/com/fredsonchaves07/moviecatchapi/domain/useCases/authentication/RecoveryPasswordUseCaseTest.java
@@ -1,8 +1,11 @@
 package com.fredsonchaves07.moviecatchapi.domain.useCases.authentication;
 
+import com.fredsonchaves07.moviecatchapi.domain.dto.authentication.LoginDTO;
 import com.fredsonchaves07.moviecatchapi.domain.dto.authentication.RecoveryPasswordDTO;
 import com.fredsonchaves07.moviecatchapi.domain.dto.token.TokenDTO;
 import com.fredsonchaves07.moviecatchapi.domain.dto.user.UserDTO;
+import com.fredsonchaves07.moviecatchapi.domain.exceptions.EmailOrPasswordIncorrectException;
+import com.fredsonchaves07.moviecatchapi.domain.exceptions.EmailOrPasswordInvalidException;
 import com.fredsonchaves07.moviecatchapi.domain.exceptions.UnconfirmedUserException;
 import com.fredsonchaves07.moviecatchapi.domain.exceptions.UserNotFoundException;
 import com.fredsonchaves07.moviecatchapi.domain.repositories.UserRepository;
@@ -37,6 +40,9 @@ public class RecoveryPasswordUseCaseTest {
 
     @Autowired
     private RecoveryPasswordUseCase recoveryPasswordUseCase;
+
+    @Autowired
+    private AuthenticateUserUseCase authenticateUserUseCase;
 
     @BeforeEach
     public void setUp() {
@@ -74,6 +80,70 @@ public class RecoveryPasswordUseCaseTest {
         RecoveryPasswordDTO recoveryPasswordDTO = new RecoveryPasswordDTO(email, newPassword);
         assertThrows(
                 UnconfirmedUserException.class,
+                () -> recoveryPasswordUseCase.execute(recoveryPasswordDTO)
+        );
+    }
+
+    @Test
+    public void shouldAuthenticateUserWithNewPassword() {
+        String email = "usertest@email.com";
+        String newPassword = "newPassword@123";
+        RecoveryPasswordDTO recoveryPasswordDTO = new RecoveryPasswordDTO(email, newPassword);
+        recoveryPasswordUseCase.execute(recoveryPasswordDTO);
+        LoginDTO loginDTO = new LoginDTO(email, newPassword);
+        TokenDTO tokenDTO = authenticateUserUseCase.execute(loginDTO);
+        assertNotNull(tokenDTO);
+    }
+
+    @Test
+    public void notShouldAuthenticateUserWithOldPassword() {
+        String email = "usertest@email.com";
+        String newPassword = "newPassword@123";
+        RecoveryPasswordDTO recoveryPasswordDTO = new RecoveryPasswordDTO(email, newPassword);
+        recoveryPasswordUseCase.execute(recoveryPasswordDTO);
+        LoginDTO loginDTO = new LoginDTO(email, "user@123");
+        assertThrows(
+                EmailOrPasswordIncorrectException.class,
+                () -> authenticateUserUseCase.execute(loginDTO)
+        );
+    }
+
+    @Test
+    public void notShouldRecoveryPasswordWithInvalidPassword() {
+        String email = "usertest@email.com";
+        String newPassword = "new";
+        RecoveryPasswordDTO recoveryPasswordDTO = new RecoveryPasswordDTO(email, newPassword);
+        assertThrows(
+                EmailOrPasswordInvalidException.class,
+                () -> recoveryPasswordUseCase.execute(recoveryPasswordDTO)
+        );
+    }
+
+    @Test
+    public void notShouldRecoveryPasswordWithEmailIsNull() {
+        String newPassword = "new";
+        RecoveryPasswordDTO recoveryPasswordDTO = new RecoveryPasswordDTO(null, newPassword);
+        assertThrows(
+                UserNotFoundException.class,
+                () -> recoveryPasswordUseCase.execute(recoveryPasswordDTO)
+        );
+    }
+
+    @Test
+    public void notShouldRecoveryPasswordWithPasswordIsNull() {
+        String email = "usertest@email.com";
+        RecoveryPasswordDTO recoveryPasswordDTO = new RecoveryPasswordDTO(email, null);
+        assertThrows(
+                EmailOrPasswordInvalidException.class,
+                () -> recoveryPasswordUseCase.execute(recoveryPasswordDTO)
+        );
+    }
+
+    @Test
+    public void notShouldRecoveryPasswordWithRecoveryPasswordDTOIsNull() {
+        RecoveryPasswordDTO recoveryPasswordDTO = new RecoveryPasswordDTO(null, null);
+        assertThrows(
+                UserNotFoundException.class,
                 () -> recoveryPasswordUseCase.execute(recoveryPasswordDTO)
         );
     }
