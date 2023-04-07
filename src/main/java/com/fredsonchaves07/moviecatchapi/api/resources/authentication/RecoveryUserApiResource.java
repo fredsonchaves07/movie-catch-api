4bc674dc5a8d03a60ad1a8331cf34939ad284756
fredsonchaves07 <fredsonchaves07@gmail.com>
2023-04-07 09:33:38 -0300

    feat: resource test implementation and feature

---
+++ src/main/java/com/fredsonchaves07/moviecatchapi/api/resources/authentication/RecoveryUserApiResource.java
@@ -0,0 +1,35 @@
+package com.fredsonchaves07.moviecatchapi.api.resources.authentication;
+
+import com.fredsonchaves07.moviecatchapi.api.services.authentication.RecoveryPasswordApiService;
+import com.fredsonchaves07.moviecatchapi.domain.dto.authentication.RecoveryPasswordDTO;
+import com.fredsonchaves07.moviecatchapi.domain.dto.user.UserDTO;
+import io.swagger.annotations.*;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/v1/recovery")
+@Api(tags = {"Recovery Password"})
+@Tag(name = "Recovery Password", description = "Recovery Password User Resource")
+public class RecoveryUserApiResource {
+
+    @Autowired
+    private RecoveryPasswordApiService service;
+
+    @PostMapping
+    @ApiOperation(value = "Authenticate a user")
+    @ApiResponses(value = {
+            @ApiResponse(code = 400, message = "Bad request"),
+            @ApiResponse(code = 401, message = "Unauthorized")
+    })
+    @ResponseStatus(value = HttpStatus.OK)
+    public ResponseEntity<UserDTO> recovery(
+            @ApiParam(name = "Recovery Password", value = "New user credentials login body")
+            @RequestBody RecoveryPasswordDTO recoveryPasswordDTO
+    ) {
+        return ResponseEntity.ok(service.execute(recoveryPasswordDTO));
+    }
+}
