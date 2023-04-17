aefe0a9fc9b773e23fe4dd25d3f1b4ad91ed15cd
fredsonchaves07 <fredsonchaves07@gmail.com>
2023-04-17 10:42:54 -0300

    refact: change email to string

--- src/main/java/com/fredsonchaves07/moviecatchapi/domain/dto/user/EmailDTO.java
+++ src/main/java/com/fredsonchaves07/moviecatchapi/domain/dto/user/EmailDTO.java
@@ -1,8 +1,13 @@
 package com.fredsonchaves07.moviecatchapi.domain.dto.user;
 
 import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
 
 @ApiModel("Email")
-public record EmailDTO(String email) {
+public record EmailDTO(@ApiModelProperty(required = true) String email) {
 
+    @Override
+    public String toString() {
+        return email;
+    }
 }
