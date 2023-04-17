91bcb308145990f6a77306dbb77f6251fefc7101
fredsonchaves07 <fredsonchaves07@gmail.com>
2023-04-17 10:42:26 -0300

    feat: include email dto response api doc swagger

--- src/main/java/com/fredsonchaves07/moviecatchapi/api/resources/user/ChangeRequestPasswordUserApiResource.java
+++ src/main/java/com/fredsonchaves07/moviecatchapi/api/resources/user/ChangeRequestPasswordUserApiResource.java
@@ -2,10 +2,7 @@ package com.fredsonchaves07.moviecatchapi.api.resources.user;
 
 import com.fredsonchaves07.moviecatchapi.api.services.user.ChangeRequestPasswordUserApiService;
 import com.fredsonchaves07.moviecatchapi.domain.dto.user.EmailDTO;
-import io.swagger.annotations.Api;
-import io.swagger.annotations.ApiOperation;
-import io.swagger.annotations.ApiResponse;
-import io.swagger.annotations.ApiResponses;
+import io.swagger.annotations.*;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -27,7 +24,10 @@ public class ChangeRequestPasswordUserApiResource {
             @ApiResponse(code = 400, message = "Bad request")
     })
     @ResponseStatus(value = HttpStatus.NO_CONTENT)
-    public ResponseEntity<Void> execute(@RequestBody EmailDTO email) {
+    public ResponseEntity<Void> execute(
+            @ApiParam(name = "Email", value = "Email user")
+            @RequestBody EmailDTO email
+    ) {
         service.execute(email);
         return ResponseEntity.noContent().build();
     }
