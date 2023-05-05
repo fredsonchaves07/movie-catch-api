4b6b24cbdc5fef0d79b7f934f8d05cc75874e32d
fredsonchaves07 <fredsonchaves07@gmail.com>
2023-05-05 10:04:14 -0300

    doc: swagger doc

--- src/main/java/com/fredsonchaves07/moviecatchapi/domain/dto/user/UpdateUserDTO.java
+++ src/main/java/com/fredsonchaves07/moviecatchapi/domain/dto/user/UpdateUserDTO.java
@@ -1,4 +1,7 @@
 package com.fredsonchaves07.moviecatchapi.domain.dto.user;
 
+import io.swagger.annotations.ApiModel;
+
+@ApiModel(value = "User update request body")
 public record UpdateUserDTO(String name, String password) {
 }
