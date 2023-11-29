be717502f114b0c68b178e1bfe235241f5ebb8f2
fredsonchaves07 <fredsonchaves07@gmail.com>
2023-11-29 16:13:46 -0300

    test: create multiple list movie user

--- src/test/java/com/fredsonchaves07/moviecatchapi/domain/useCases/listUser/CreateSerieMovieListUserUseCaseTest.java
+++ src/test/java/com/fredsonchaves07/moviecatchapi/domain/useCases/listUser/CreateSerieMovieListUserUseCaseTest.java
@@ -76,4 +76,59 @@ public class CreateSerieMovieListUserUseCaseTest {
         assertFalse(listMovieUser.movieUserDTOList().isEmpty());
         assertEquals(2, listMovieUser.movieUserDTOList().size());
     }
+
+    @Test
+    public void shouldCreateMultipleListMovieUser() {
+        CreateListMovieUserDTO list1 = new CreateListMovieUserDTO(
+                "List movie user 1", "List movie user 1 description", List.of(
+                new MovieUserDTO("1", "Movie 1", "urlImageMovie1"),
+                new MovieUserDTO("2", "Serie 1", "urlImageSerie1")
+        )
+        );
+        CreateListMovieUserDTO list2 = new CreateListMovieUserDTO(
+                "List movie user 2", "List movie user 2 description", List.of(
+                new MovieUserDTO("6", "Serie 6", "urlImageSerie6"),
+                new MovieUserDTO("10", "Movie 10", "urlImageMovie10")
+        )
+        );
+        CreateListMovieUserDTO list3 = new CreateListMovieUserDTO(
+                "List movie user 3", "List movie user 3 description", List.of(
+                new MovieUserDTO("2", "Serie 1", "urlImageSerie1"),
+                new MovieUserDTO("1", "Movie 1", "urlImageMovie1")
+        )
+        );
+        ListMovieUserDTO listMovieUser1 = createSerieMovieListUserUseCase.execute(list1);
+        ListMovieUserDTO listMovieUser2 = createSerieMovieListUserUseCase.execute(list2);
+        ListMovieUserDTO listMovieUser3 = createSerieMovieListUserUseCase.execute(list3);
+        assertNotNull(listMovieUser1);
+        assertNotNull(listMovieUser2);
+        assertNotNull(listMovieUser3);
+        assertEquals("List movie user 1", listMovieUser1.name());
+        assertEquals("List movie user 1 description", listMovieUser1.description());
+        assertEquals(userDTO, listMovieUser1.user());
+        assertEquals(List.of(
+                new MovieUserDTO("1", "Movie 1", "urlImageMovie1"),
+                new MovieUserDTO("2", "Serie 1", "urlImageSerie1")
+        ), listMovieUser1.movieUserDTOList());
+        assertFalse(listMovieUser1.movieUserDTOList().isEmpty());
+        assertEquals(2, listMovieUser1.movieUserDTOList().size());
+        assertEquals("List movie user 2", listMovieUser2.name());
+        assertEquals("List movie user 2 description", listMovieUser2.description());
+        assertEquals(userDTO, listMovieUser2.user());
+        assertEquals(List.of(
+                new MovieUserDTO("6", "Serie 6", "urlImageSerie6"),
+                new MovieUserDTO("10", "Movie 10", "urlImageMovie10")
+        ), listMovieUser2.movieUserDTOList());
+        assertFalse(listMovieUser2.movieUserDTOList().isEmpty());
+        assertEquals(2, listMovieUser2.movieUserDTOList().size());
+        assertEquals("List movie user 3", listMovieUser3.name());
+        assertEquals("List movie user 3 description", listMovieUser3.description());
+        assertEquals(userDTO, listMovieUser3.user());
+        assertEquals(List.of(
+                new MovieUserDTO("2", "Serie 1", "urlImageSerie1"),
+                new MovieUserDTO("1", "Movie 1", "urlImageMovie1")
+        ), listMovieUser3.movieUserDTOList());
+        assertFalse(listMovieUser3.movieUserDTOList().isEmpty());
+        assertEquals(2, listMovieUser3.movieUserDTOList().size());
+    }
 }
